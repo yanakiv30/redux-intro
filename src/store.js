7f8e4013dab36289b2f7cb3dcc84624f444ed43a
yanakiv30 <yanakiv30@gmail.com>
2024-02-27 16:32:12 +0200

    loanPurpose

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -11,9 +13,12 @@ function reducer(state = initialState, action) {
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
-      if (state.loan > 0) return state;
-      //Later  loanPurpose: "Buy a car"
-      return { ...state, loan: action.payload };
+      if (state.loan > 0) return state;      
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -25,3 +30,12 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+store.dispatch({ type: "account/deposit", payload: 500 });
+store.dispatch({ type: "account/withdraw", payload: 200 });
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "Buy a car" },
+});
+console.log(store.getState());
