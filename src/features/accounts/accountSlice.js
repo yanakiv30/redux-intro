fb03d18029fa47b72d7d472314ecc34392d9e6a5
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-03-01 07:08:16 +0200

    payLoan

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -16,21 +16,31 @@ const accountSlice = createSlice({
     withdraw(state, action) {
       state.balance -= action.payload;
     },
-    requestLoan(state, action) {
-      if (state.loan > 0) return;
-      state.loan = action.payload.amount;
-      state.loanPurpose = action.payload.purpose;
-      state.balance += action.payload.amount;
+    requestLoan: {
+      prepare(amount, purpose) {
+        return { payload: { amount, purpose } };
+      },
+
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance += action.payload.amount;
+      },
     },
     payLoan(state, action) {
+      state.balance -= state.loan;
       state.loan = 0;
       state.purpose = "";
-      state.balance -= state.loan;
+      
     },
   },
 });
-console.log(accountSlice);
-export const {deposit, payLoan, requestLoan, withdraw} = accountSlice.actions;
+
+export const { deposit, payLoan, requestLoan, withdraw } = accountSlice.actions;
+
+
+
 export default accountSlice.reducer;
 
 // export default function accountReducer(state = initialStateAccount, action) {
